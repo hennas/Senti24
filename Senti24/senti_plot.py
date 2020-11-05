cdde3ee034fe588c38fd8a9a4f987f5628f11ea0
Zabrakk <32382184+Zabrakk@users.noreply.github.com>
2020-11-05 15:46:05 +0200

    Reformatting code

--- Senti24/senti_plot.py
+++ Senti24/senti_plot.py
@@ -1,8 +1,8 @@
 import time
-import json
 import logging
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
 
 class SentiPlot:
@@ -17,15 +17,6 @@ class SentiPlot:
         """
         return sum(senti_sum)/len(senti_sum)
 
-    def month_avg_nonneutral(self, senti_sum: list) -> float:
-        """
-        Calculates the average monthly sentiment excluding sentiment sums of 0
-        :param senti_sum: List of sentiment sums
-        :return: The average sentiment
-        """
-        senti_sum = [i for i in senti_sum if i != 0]
-        return sum(senti_sum) / len(senti_sum)
-
     def draw(self, data: 'DataFrame', save: bool = False):
         """
         Draws a plot of the average sentiment for each year/month in the given data
@@ -33,38 +24,64 @@ class SentiPlot:
         """
         self.logger.info('Calculating averages for each month')
         start = time.time()
-        obs = {}  # Sentiment observations with 0s
-        obs_nonneutral = {}  # Same, but excluding 0 sums
+        obs = {}  # Sentiment observations
         # Extract unique years
         for year in data['year'].unique():
             # Calculate average sentiment for each month
             for month in data[data['year'] == year]['month'].unique():
-            #for month in list(set(data[data['year'] == year]['month'].values)):
                 avgs = data[(data['year'] == year) & (data['month'] == month)]['senti_avg'].values
                 obs[f'{year}-{month:02d}'] = sum(avgs)/len(avgs)
-                # self.month_avg(data[(data['year'] == year) & (data['month'] == month)]['s_sum'].values)
-                # obs_nonneutral[f'{year}-{month:02d}'] = self.month_avg_nonneutral(data[(data['year'] == year)&(data['month'] == month)]['s_sum'].values)
         self.logger.info(f'Averages calculated, took {time.time()-start}s')
         # Save the result
         if save:
             self.save_stats(obs)
-        # open('senti_avg.json', 'w').write(json.dumps(obs, indent=4))
         self.logger.info('Drawing the plot')
         # Creating the figure
         fig = plt.figure()
         ax = plt.subplot(111)
         # Adding sentiments to the plot
         obs_range = range(len(list(obs.keys())))
-        # ax.bar(obs_range, list(obs_nonneutral.values()), color='darkgray', width=0.7, edgecolor='k', label='Non-neutral')
-        ax.bar(obs_range, list(obs.values()), width=0.7, color='grey', edgecolor='k', label='All sentimets')
+        ax.plot(obs_range, list(obs.values()), color='blue', linewidth=4)
         # Adding ticks and labels
-        plt.xticks(obs_range, list(obs.keys()), rotation=70)
-        plt.ylabel('Average sentiment')
-        plt.xlabel('Date')
+        labels = list(obs.keys())
+        for i in range(1, len(labels), 2):
+            labels[i] = ''
+        plt.xticks(obs_range, labels, rotation=50)
+        plt.title('Average sentiment scores on a monthly basis', fontsize=20)
+        plt.ylabel('Average sentiment', fontsize=10)
+        plt.xlabel('Date', fontsize=10)
         ax.legend()
         # Showing the plot
         plt.show()
         self.logger.info('Drawing complete')
+
+    def calculate_averages(self, db: pd.DataFrame) -> dict:
+        # Extract unique years
+        obs = {}
+        for year in db['year'].unique():
+            # Calculate average sentiment for each month
+            for month in db[db['year'] == year]['month'].unique():
+                avgs = db[(db['year'] == year) & (db['month'] == month)]['senti_avg'].values
+                obs[f'{year}-{month:02d}'] = sum(avgs) / len(avgs)
+        return obs
+
+    def draw_to_gui(self, db: pd.DataFrame):
+        self.logger.info('Drawing figure for the GUI')
+        obs = self.calculate_averages(db)  # Sentiment observations
+        fig = Figure(dpi=300)
+        axis = fig.add_subplot(1, 1, 1)
+        obs_range = range(len(list(obs.keys())))
+        axis.plot(obs_range, list(obs.values()), color='blue', linewidth=2.5)
+        labels = list(obs.keys())
+        for i in range(1, len(labels), 2):
+            labels[i] = ''
+        axis.set_title('Average sentiment scores on a monthly basis')
+        axis.set_xticks(range(len(labels)))
+        axis.set_xticklabels(labels, rotation=90, fontsize=7)
+        axis.set_xlabel('Date', fontsize=18)
+        axis.set_ylabel('Average sentiment', fontsize=18)
+        self.logger.info('Done drawing')
+        return fig
 
     def save_stats(self, stats: dict):
         self.logger.info('Saving stats')
